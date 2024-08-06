ce10dac8f706acf6468012139e10bcf245c70ee9
arminabdijs <arminabdijs@gmail.com>
2024-08-06 03:58:43 +0330

    update file js

---
+++ index.js
@@ -0,0 +1,76 @@
+//This will open a window and print a hello world message
+// alert("hello world");
+
+// ------------------------------------------------
+// Several types of methods for variable definition
+// var c = 13;
+// let a = 13;
+// const b = 13;
+
+// ------------------------------------------------
+// typeof will return the type of my variable
+// var c = 13;
+// let a = "13";
+// const b = true;
+// var g = 13n;
+
+// alert(typeof c); //type number
+// alert(typeof a); //type string
+// alert(typeof b); //type boolean
+// alert(typeof g); //type bigint
+
+// ------------------------------------------------
+// With this method, we can get input from the user
+// const your_Name=prompt("enter your Name :")
+// alert(your_Name)
+
+// const your_Age=prompt("enter your Age :")
+// const result=Number(your_Age)
+// alert(result)
+
+// const your_Age=prompt("enter your Age :")
+// const result=+your_Age
+// alert(result)
+
+// const your_Age2=Number(prompt("enter your Age"))
+// alert(your_Age2)
+
+// const your_Name2=prompt("enter your Name :","Armin Abdi")
+// alert(your_Name)
+
+// alert(Number(prompt("enter your Age")))
+
+// ------------------------------------------------
+// type NaN => Not a Number
+// alert(Number("Armin Abdi"))
+
+// ------------------------------------------------
+// Data type conversion
+// let boolean = true;
+// let string = String(boolean)
+// alert(typeof string)
+
+// ------------------------------------------------
+// Mathematical operations of addition, subtraction, multiplication and...
+// let num1=2
+// let num2=2
+
+// alert(num1+num2)
+// alert(num1-num2)
+// alert(num1*num2)
+// alert(num1**num2)
+// alert(num1%num2)
+
+// ------------------------------------------------
+// workout
+// let num1=Number(prompt("enter your Number"))
+// let num2=Number(prompt("enter your Number"))
+
+// alert(num1+num2)
+// alert(num1-num2)
+// alert(num1*num2)
+// alert(num1**num2)
+// alert(num1%num2)
+
+// ------------------------------------------------
+
