1f9fdd7e85d0e2e2e26b2ea745987a0842dfa6a5
arminabdijs <arminabdijs@gmail.com>
2024-09-02 14:06:41 +0330

    update file

--- index.js
+++ index.js
@@ -369,3 +369,43 @@
 
 // ------------------------------------------------
 // workout
+// let userAge = Number(prompt("enter your Age : "));
+
+// let welcome;
+
+// if (userAge < 18) {
+//   welcome = function () {
+//     alert("Hello");
+//   };
+// } else {
+//   welcome = function () {
+//     alert("Praise");
+//   };
+// }
+
+// welcome();
+
+// ------------------------------------------------
+// Arrow functions
+// let double = (n) => n * 2;
+// alert(double(3));
+
+// let sum = (a, b) => {
+//   let result = a + b;
+//   return result;
+// };
+
+// alert(sum(1, 2));
+
+// ------------------------------------------------
+// workout
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+// ask(
+//   "Do you agree?",
+//   () => alert("You agreed."),
+//   () => alert("You canceled the execution.")
+// );
