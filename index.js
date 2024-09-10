20d4eeedc4a20005d455c57b4203376e51bdfc23
arminabdijs <arminabdijs@gmail.com>
2024-09-10 11:17:47 +0330

    update JS file

--- index.js
+++ index.js
@@ -529,4 +529,220 @@
 //   String(prompt("Enter Password : ", "ArminAbdi2017"))
 // );
 // ------------------------------------------------
+// Math methods
+// console.log(Math.PI); //return number pi (3.141592653589793)
+
+// console.log(Math.pow(2, 4)); // return 2**4
+
+// console.log(Math.sqrt(16));// return It takes the square root of 16
+
+// console.log(Math.abs(-1)); //It takes the absolute value of -1
+
+// console.log(Math.min(1, 2, 3, 4, 56, 6, 7, 889, 9, 2, -1)); //Returns the smallest number
+
+// console.log(Math.max(1, 2, 3, 4, 56, 6, 7, 889, 9, 2, -1)); //Returns the largest number
+
+// console.log(Math.floor(1.9)); //This will remove the decimal part and return the correct part
+
+// console.log(Math.trunc(2.6)); //This will remove the decimal part and return the correct part
+
+// console.log(Math.round(12.7)); //This is going to change our number
+
+// console.log(Math.random() * 10); //It returns a random number between 0 and 1. If we want to get a specific number, we multiply it.
+
+// console.log(Math.acos(1)); //This field returns the Arkcosine of 1
+
+// console.log(Math.asin(1)); //This will return the Arksine of 1
+
+// console.log(Math.atan(1)); //This field returns the Arktangent of 1
+
+// console.log(Math.atan2(2, 6)); //This field returns the Arktangents2 of two points x and y
+
+// console.log(Math.ceil(2.1)); //This rends any number that is out of the integer, for example, it rends the number 2.1 to 3.
+
+// console.log(Math.exp(3)); //It will multiply e=2.71 by any number we give it
+
+// console.log(Math.log(9)); //Returns the natural logarithm (base e=2.71) of a number
+
+// console.log(Math.sin(9999999999999999));//Sine returns a number between 1 and -1
+
+// console.log(Math.tan(9999999999999999));//tangent returns a number between 1 and -1
+
+// console.log(Math.cos(9999999999999999));//cosine returns a number between 1 and -1
+
+// console.log(Math.E); //This is Euler's number
+
+// console.log(Math.LN10); //LN10===log(10)
+
+// ------------------------------------------------
+// workout
+// let firstRandumNumber = Math.random() * 100000;
+// let finalCaptchaCode = Math.floor(firstRandumNumber);
+
+// console.log(`your Captcha Code : ` + finalCaptchaCode);
+
+// ------------------------------------------------
+// Assignment Operators
+// let number = 10;
+
+// number += 5;
+
+// number++
+
+// ++number
+
+// number -= 10;
+
+// number--
+
+// --number
+
+// number *= 7;
+
+// number **= 2;
+
+// number /= 9;
+
+// console.log(number);
+
+// ------------------------------------------------
+// x++ VS ++x
+// let num1 = 0;
+
+// let num2 = num1++;
+
+// let num2 = ++num1;
+
+// console.log("num2 : " + num2);
+// console.log("num1 : " + num1);
+
+// ------------------------------------------------
+// Loops: while
+// let firstNumber = 0;
+// while (firstNumber < 5) {
+//   console.log("Hello world!");
+//   firstNumber++;
+// }
+
+// let firstNumber = 0;
+// while (firstNumber < 3) console.log(firstNumber++);
+
+// let firstNumber = 0;
+// do {
+//   console.log("Hi world!");
+//   firstNumber++;
+// } while (firstNumber < 5);
+
+// ------------------------------------------------
+// Loops: for
+// for (let i = 0; i < 5; i++) {
+//   console.log("Hello World!");
+// }
+
+// let i = 0;
+// for (; i < 3; ) {
+//   console.log(i++);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i % 2 === 0) continue;
+//   console.log(i);
+// }
+
+// label: for (let i = 0; i < 10; i++) {
+//   if (i % 2) {
+//     for (i; i > 5; ) {
+//       break label;
+//       console.log("hello");
+//     }
+//   }
+// }
+
+// ------------------------------------------------
+// Loops: for in
+// let user = {
+//   name: "armin",
+//   family: "abdi",
+//   age: 22,
+// };
+// for (const key in user) {
+//   console.log(key);
+//   console.log(user[key]);
+//   console.log("--------------------------");
+// }
+
+// ------------------------------------------------
+// Loops: for of
+// let arr = [
+//   "html",
+//   "css",
+//   "js",
+//   "angular",
+//   "react",
+//   "nodejs",
+//   "mongoDB",
+//   "MYSQL",
+// ];
+
+// for (const element of arr) {
+//   console.log(element,"GOOD");
+// }
+
+// ------------------------------------------------
+// workout
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`number is ${i} even`);
+//   }
+// }
+
+// ------------------------------------------------
+// workout
+// let allPrices = 0;
+
+// for (let i = 0; i < 5; i++) {
+//   allPrices += Number(prompt("Enter the product price: "));
+// }
+
+// alert("All price of your basket: " + allPrices);
+
+// ------------------------------------------------
+// workout
+// let userNumbser = 0;
+// let sumNumberOfNumbers = 0;
+// for (let i = 1; i <= 5; i++) {
+//   userNumbser += +prompt(`enter ${i} number`);
+//   sumNumberOfNumbers++;
+// }
+
+// alert(userNumbser / sumNumberOfNumbers);
+
+// ------------------------------------------------
+// workout
+// let userNumber = Number(prompt("Enter the Number : ", 0));
+// let sum = 0;
+
+// for (let i = 0; userNumber / 10 != 0; i++) {
+//   sum += userNumber % 10;
+//   userNumber = Math.floor(userNumber / 10);
+// }
+
+// console.log("Sum Of Digits : ", sum);
+
+// ------------------------------------------------
+// workout
+// let userNumber = Number(prompt("Enter the Number : ", 0));
+// let sum = 0;
+// if (userNumber === 0) {
+//   sum = 1;
+// } else {
+//   for (let i = 0; userNumber / 10 != 0; i++) {
+//     sum++;
+//     userNumber = Math.floor(userNumber / 10);
+//   }
+// }
+
+// console.log("Sum Of Digits : ", sum);
+
+// ------------------------------------------------
 // workout
