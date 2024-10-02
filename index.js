707bc6fa9b5d55910ea72911e42b850b9c0f9817
arminabdijs <arminabdijs@gmail.com>
2024-10-02 12:35:44 +0330

    update file js

--- index.js
+++ index.js
@@ -96,6 +96,8 @@
 
 // alert('armin "abdi"');
 
+// alert("armin\"abdi\"")
+
 // ------------------------------------------------
 // Comparative operators
 // let num1 = 12;
@@ -126,7 +128,7 @@
 //   alert("Oops");
 // }
 
-// This is also a type of condition that stores the answer in a variable. It compares two values. If it is true, it stores the first item after the knowledge of the question in the variable, and if it is false, it stores the false in the variable.
+// This is also a type of "condition operator" that stores the answer in a variable. It compares two values. If it is true, it stores the first item after the knowledge of the question in the variable, and if it is false, it stores the false in the variable.
 // let age = 12;
 // let accessAllowed = age > 18 ? true : false;
 // alert(accessAllowed); // false
@@ -138,7 +140,7 @@
 // if (inputNumber % 2 === 0) {
 //   alert("Entered is even");
 // } else if (inputNumber % 2 === 1) {
-//   alert("Entered is a person");
+//   alert("Entered is a odd");
 // } else {
 //   alert("Please enter a number");
 // }
@@ -351,6 +353,8 @@
 
 // ask("do you agree ? ", showOk, showCancel);
 
+// ------------------------------------------------
+// workout
 // function showCount(count) {
 //   alert(count ?? "unknown");
 // }
@@ -358,6 +362,7 @@
 // showCount(0); // 0
 // showCount(null); // unknown
 // showCount(); // unknown
+// showCount("Hello")
 
 // ------------------------------------------------
 // workout
@@ -486,7 +491,7 @@
 
 //It goes into the text, separates it from the start index to the end index and returns it to us
 // let text = "I love JavaScript";
-// console.log(text.slice(7, 17));
+// console.log(text.slice(-10, 17));
 
 // This comes into the text, starting from the index to the length of the letters we want to return
 // let text = "I love JavaScript";
@@ -515,8 +520,9 @@
 // workout
 // function loginForm(userName, userPassword) {
 //   if (
-//     (userName.length >= 3 && userPassword.length >= 8,
-//     userName.toLowerCase() === "ali")
+//     userName.length >= 3 &&
+//     userPassword.length >= 8 &&
+//     userName.toLowerCase() === "ali"
 //   ) {
 //     alert("You are very welcome");
 //   } else {
@@ -746,3 +752,199 @@
 
 // ------------------------------------------------
 // workout
+// let userNumberStart = Number(prompt("Enter your min number : "));
+// let userNumberEnd = Number(prompt("Enter your max number : "));
+
+// if (userNumberStart <= userNumberEnd) {
+//   while (userNumberStart <= userNumberEnd) {
+//     if (userNumberStart % 2 === 0) {
+//       console.log(userNumberStart);
+//     }
+//     userNumberStart++;
+//   }
+// } else {
+//   while (userNumberEnd <= userNumberStart) {
+//     if (userNumberStart % 2 === 0) {
+//       console.log(userNumberStart);
+//     }
+//     userNumberStart--;
+//   }
+// }
+
+// ------------------------------------------------
+// workout
+// let enterTheNumberYouWant = Number(prompt("Enter the number you want : "));
+// let userNumbser = 0;
+// let sumNumberOfNumbers = 0;
+// let i = 1;
+
+// while (i <= enterTheNumberYouWant) {
+//   userNumbser += Number(prompt(`entter ${i} number : `));
+//   sumNumberOfNumbers++;
+//   i++;
+// }
+// console.log(sumNumberOfNumbers);
+// console.log(userNumbser);
+
+// alert(userNumbser / sumNumberOfNumbers);
+
+// ------------------------------------------------
+// workout
+// let enterNumber1 = Number(prompt("Enter 1 number : "));
+// let enterNumber2 = Number(prompt("Enter 2 number : "));
+
+// if (enterNumber1 <= 0 || enterNumber2 <= 0) {
+//   alert("numbers not 0 and -1,-2,-3,....");
+// }
+
+// while (enterNumber1 > 0 && enterNumber2 > 0) {
+//   let sum = enterNumber1 ** enterNumber1;
+//   alert(sum);
+//   break;
+// }
+
+// ------------------------------------------------
+// Array
+// let fruits = ["Apple", "Orange", "Plum"];
+// console.log(fruits[0]); //This returns the index 0 of that array
+// console.log(fruits[1]);
+
+// fruits[1] = "black"; //This will change the index 1 of that array
+// console.log(fruits[1]);
+
+// console.log(fruits[fruits.length - 1]); //It returns the last item of the array with the help of length minus 1
+
+// console.log(fruits.at(-1)); //It returns the last item using the at method
+
+// console.log(fruits.pop()); //This removes the last item of the array with the help of the pop method
+// console.log(fruits);
+
+// fruits.push("armin"); //This adds an item to the end of the array using the push method
+// console.log(fruits);
+
+// console.log(fruits.shift()); //This removes an item from the beginning of the array with the help of the shift method
+// console.log(fruits);
+
+// fruits.unshift("javaScript"); //This adds an item to the beginning of the array using the unshift method
+// console.log(fruits);
+
+// let arr = new Array("Apple", "Pear", "etc");
+// let arr2 = new Array(2); //This type of array has a problem with numbers
+// console.log(arr2[0]);
+// console.log(arr2.length);
+
+// let matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ]; //Multiple arrays
+// console.log(matrix[1][1]);
+
+// let arrNmber = [1, 2, 3];
+// console.log(String(arrNmber) === "1,2,3");
+
+// let arr = ["I", "go", "home"];
+// delete arr[1]; //The delete method is for objects and it deletes that item in the array, but the desired place is still occupied.
+// console.log(arr[1]);
+
+// let arr = ["I", "study", "JavaScript"];
+// arr.splice(0, 2); //The splice method removes or separates your items from the desired index to the number you give it
+// console.log(arr);
+
+// let arr = ["I", "study", "JavaScript"];
+// arr.splice(0, 2, "armin"); //Here, it removes items from index 0 to 2 and replaces them with armin
+// console.log(arr);
+
+// let arr = ["I", "study", "JavaScript"];
+// arr.splice(0, 0, "armin"); //Here it adds an item from index 0
+// console.log(arr);
+
+// let arr=[1,2,5]
+// arr.splice(-1,0,3,4)
+// console.log(arr);
+
+// let arr = [1, 2, 5];
+// let copyArr = arr.slice(0, 1 + 1); //This method copies from the start index to the end index into another variable
+// let copyArr2 = arr.slice(-2);
+// console.log(copyArr);
+// console.log(copyArr2);
+// console.log(arr);
+
+// let arr=[1,2]
+// console.log(arr.concat(3,4));
+// console.log(arr.concat([5,6],[7,8]));
+
+// ["Armin", "Ali", "mohammad"].forEach(console.log); //The arr.forEach method allows us to run a function on all elements of an array.
+
+// ["Armin", "Ali", "mohammad"]
+//   .forEach((itme, index, array) => {
+//     console.log(`${itme} is index ${index} in array ${array}`);
+//   })
+
+// let arr = [1, 2, 3, "Armin", true];
+// console.log(arr.indexOf("Armin")); //This will return the index of that item and if the item does not exist, it will return -1
+// console.log(arr.includes(3)); //This returns true if the item exists and false if not
+// console.log(arr.find(1));
+
+// let arr = [1, 2, 3, "Armin", true];
+// let arr2 = [ "Armin", "true","ali","mohammad"];
+// console.log(arr.sort((a, b) => (a > b ? 1 : -1)));
+// console.log(arr2.sort( (a, b) => a.localeCompare(b) ));//this is sorted by string
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.reverse()); //This will reverse the array
+
+// let test = "armin";
+// console.log(test.split("")); //output => [ 'a', 'r', 'm', 'i', 'n' ]
+
+// let arr = ["armin", "ali", "mohammad", "sara"];
+// console.log(arr.splice(",", 2)); //output => [ 'armin', 'ali' ]
+
+// let arr = ['Bilbo', 'Gandalf', 'Nazgul'];
+// console.log(arr.join(" ; "));//output => Bilbo ; Gandalf ; Nazgul
+
+// let a = ["1", "2"];
+// a.push(function () {
+//   console.log(this);
+// });
+// a[2]();
+
+// ------------------------------------------------
+// workout
+// let i = 0;
+// let enterTimeNumber = Number(prompt("how enter time your number "));
+// let arrNumbers = [];
+// let sum = 0;
+
+// while (i < enterTimeNumber) {
+//   let enterNumber = Number(prompt("how enter your number "));
+//   arrNumbers.push(enterNumber);
+//   i++;
+// }
+
+// for (const arrNmber of arrNumbers) {
+//   sum += arrNmber;
+// }
+
+// alert(sum / arrNumbers.length);
+
+// ------------------------------------------------
+// workout
+// var userNumbers = []  // [12, 89, 13, 65, 21, 90]
+
+// var userNumber = 0
+// var sum = 0
+
+// while (userNumber != -1) {
+// 	userNumber = Number(prompt('Enter the number: \n enter -1 if you dont enter any number'))
+// 	if (userNumber != -1) {
+// 		userNumbers.push(userNumber)
+// 	}
+// }
+
+// for (let arrNmber1 of userNumbers) {
+//   sum += arrNmber1;
+// }
+
+// console.log(userNumbers)
+// console.log("Average: ", sum / userNumbers.length)
